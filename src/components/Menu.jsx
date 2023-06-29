d5ef50284c82c28b79a18ecc67111006f9d47cde
Naimur Reza <naimur.rezaa@gmail.com>
2023-06-29 18:07:11 +0600

    fixed menu height and width

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 import { motion } from "framer-motion";
-import { fadeIn } from "../utils/motion";
-import SectionWrapper from "../hoc/SectionWrapper";
+import { fadeIn, staggerContainer } from "../utils/motion";
+
+const StarWrapper = (Component, idName) =>
+  function HOC() {
+    return (
+      <motion.section
+        variants={staggerContainer()}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: true, amount: 0.25 }}
+        className={`${"sm:px-16 px-6 sm:py-6 py-8"} max-w-7xl mx-auto    `}>
+        <span className="hash-span" id={idName}>
+          &nbsp;
+        </span>
+
+        <Component />
+      </motion.section>
+    );
+  };
+
 const Menu = () => {
   return (
-    <motion.div variants={fadeIn("top", "spring", 0.5, 1.4)}>
+    <motion.div variants={fadeIn("top", "spring", 0.5, 2)}>
       <div className="flex    justify-between">
         <h1 className="text-3xl font-bold pink-text-gradient border-secondary border-2 px-2  rounded-full">
           N
@@ -28,4 +46,4 @@ const Menu = () => {
   );
 };
 
-export default SectionWrapper(Menu, "");
+export default StarWrapper(Menu, "");
