67929f0dd5f903129811bfef91cf25f7a0db55a4
Naimur Reza <naimur.rezaa@gmail.com>
2023-06-29 17:22:36 +0600

    added animation in about part

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,10 @@ import React, { useEffect, useState } from "react";
 import { styles } from "../style";
 import moment from "moment/moment";
 import { socialLinks } from "../constant";
+import { motion } from "framer-motion";
 import { IconContext } from "react-icons";
+import { fadeIn, slideIn } from "../utils/motion";
+import SectionWrapper from "../hoc/SectionWrapper";
 const About = () => {
   const [greetings, setGreetings] = useState("");
   const currentHour = moment().hour();
@@ -21,29 +24,33 @@ const About = () => {
   }, []);
   return (
     <>
-      <h1 className={`${"orange-text-gradient"}  mt-7`}>{greetings}</h1>
       <div className="flex justify-between pt-[6%] ">
-        <div>
-          <p className={`${styles.heroHeadText} `}>
-            Hi, I'm <span className="blue-text-gradient">Naimur</span>
-          </p>
-          <p className={`${styles.heroSubText} w-2/3`}>
-            I develop web interface and web application
-          </p>
-        </div>
+        <motion.div variants={fadeIn("right", "spring", "0.4", "1.5")}>
+          <h1 className={`${"orange-text-gradient"}  mt-7`}>{greetings}</h1>
+          <div>
+            <p className={`${styles.heroHeadText} `}>
+              Hi, I'm <span className="blue-text-gradient">Naimur</span>
+            </p>
+            <p className={`${styles.heroSubText} w-2/3`}>
+              I develop web interface and web application
+            </p>
+          </div>
+        </motion.div>
         <div className="text-sky-400 opacity-80 space-y-5 mt-8">
           {socialLinks.map((socialLink, index) => {
             return (
-              <div key={index}>
-                <IconContext.Provider value={{ className: "icon-class" }}>
-                  <a href={socialLink.link}>
-                    <socialLink.icon
-                      size={26}
-                      className="hover:-translate-y-1 duration-200 transition cursor-pointer"
-                    />
-                  </a>
-                </IconContext.Provider>
-              </div>
+              <motion.div variants={fadeIn("left", "spring", "0.4", "1.5")}>
+                <div key={index}>
+                  <IconContext.Provider value={{ className: "icon-class" }}>
+                    <a href={socialLink.link}>
+                      <socialLink.icon
+                        size={26}
+                        className="hover:-translate-y-1 duration-200 transition cursor-pointer"
+                      />
+                    </a>
+                  </IconContext.Provider>
+                </div>
+              </motion.div>
             );
           })}
         </div>
@@ -52,4 +59,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default SectionWrapper(About, "");
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -8,24 +8,26 @@ import ServiceCard from "./ServiceCard";
 const Services = () => {
   return (
     <>
-      <motion.div variants={textVariant()}>
-        <p className={styles.sectionSubText}>Introduction</p>
-        <h2 className={styles.sectionHeadText}>Overview.</h2>
-      </motion.div>
+      <div className="mt-20">
+        <motion.div variants={textVariant()}>
+          <p className={styles.sectionSubText}>Introduction</p>
+          <h2 className={styles.sectionHeadText}>Overview.</h2>
+        </motion.div>
 
-      <motion.p
-        variants={fadeIn("", "", 0.1, 1)}
-        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]">
-        I'm a skilled software developer with experience in TypeScript and
-        JavaScript, and expertise in frameworks like React, Node.js, and
-        Three.js. I'm a quick learner and collaborate closely with clients to
-        create efficient, scalable, and user-friendly solutions that solve
-        real-world problems. Let's work together to bring your ideas to life!
-      </motion.p>
-      <div className="mt-20 flex flex-wrap gap-10">
-        {services.map((service, index) => (
-          <ServiceCard key={service.title} index={index} {...service} />
-        ))}
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]">
+          I'm a skilled software developer with experience in TypeScript and
+          JavaScript, and expertise in frameworks like React, Node.js, and
+          Three.js. I'm a quick learner and collaborate closely with clients to
+          create efficient, scalable, and user-friendly solutions that solve
+          real-world problems. Let's work together to bring your ideas to life!
+        </motion.p>
+        <div className="mt-20 flex flex-wrap gap-10">
+          {services.map((service, index) => (
+            <ServiceCard key={service.title} index={index} {...service} />
+          ))}
+        </div>
       </div>
     </>
   );
