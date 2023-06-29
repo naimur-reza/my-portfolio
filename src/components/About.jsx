b38fcbd66644a4dd8e4582c73e1fcd8235c95346
Naimur Reza <naimur.rezaa@gmail.com>
2023-06-29 14:22:47 +0600

    added greetings feature using moment js

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,17 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { styles } from "../style";
+import moment from "moment/moment";
 const About = () => {
+  const [greetings, setGreetings] = useState("");
+  const currentHour = moment().hour();
+  console.log(currentHour);
+  useEffect(() => {
+    if (currentHour >= 5 && currentHour < 12) {
+      setGreetings("Good Morning 🌅");
+      return;
+    } else if (currentHour >= 12 && currentHour < 18) {
+      setGreetings("Good Afternoon ☀️");
+      return;
+    } else {
+      setGreetings("Good Evening 🌙");
+      return;
+    }
+  }, []);
   return (
     <>
-      <div className="pt-[10%]">
-        <p className={`${styles.heroHeadText} `}>
-          Hi, I'M <span className="blue-text-gradient">Naimur</span>
-        </p>
-        <p className={`${styles.heroSubText} w-1/3`}>
-          I develop web interface and web application
-        </p>
+      <h1 className={`${styles.heroSubText} red-text-gradient mt-7`}>
+        {greetings}
+      </h1>
+      <div>
+        <div className="pt-[7%]">
+          <p className={`${styles.heroHeadText} `}>
+            Hi, I'm <span className="blue-text-gradient">Naimur</span>
+          </p>
+          <p className={`${styles.heroSubText} w-1/3`}>
+            I developn web interface and web application
+          </p>
+        </div>
+        <div></div>
       </div>
-      nice with
     </>
   );
 };
