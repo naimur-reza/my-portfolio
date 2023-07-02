5063c0329bde0a8eb97e15065490160e1f177a0a
Naimur Reza <naimur.rezaa@gmail.com>
2023-07-02 12:50:53 +0600

    added new contact button animation

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -13,7 +13,7 @@ const Contact = () => {
       <div className="container gap-4   mx-auto flex items-center justify-between lg:flex-row flex-col-reverse">
         <div className="mt-5 lg:w-2/3 w-full ">
           <motion.div variants={fadeIn("up", "spring", 0.5, 0.75)}>
-            <div className="w-full px-8 py-8 mx-auto overflow-hidden   rounded-lg shadow-2xl   bg-white/5  backdrop-blur-sm  ">
+            <div className="w-full px-8 py-8 mx-auto overflow-hidden   rounded-lg shadow-2xl     backdrop-blur-sm  ">
               <form className="mt-6">
                 <div className="flex-1">
                   <label className="block mb-2 text-sm      text-gray-200">
@@ -43,13 +43,9 @@ const Contact = () => {
                     className="block w-full h-32 px-5 py-3 mt-2      border   rounded-md md:h-48   placeholder-gray-600   bg-gray-900   text-gray-300   border-gray-700    focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
                     placeholder="Message"></textarea>
                 </div>
-                <a href="#" class="animated-button1">
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                  Button
-                </a>
+                <button class="button-85" role="button">
+                  Send Message
+                </button>
               </form>
             </div>
           </motion.div>
