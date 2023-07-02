1daae9b1bae88da0e963768959c52a3d1d7bb6da
Naimur Reza <naimur.rezaa@gmail.com>
2023-07-02 17:35:17 +0600

    added email js functionality

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useRef } from "react";
 import EmailLottie from "./EmailLottie";
 import { motion } from "framer-motion";
 import { fadeIn } from "../utils/motion";
 import StarWrapper from "../hoc/SectionWrapper";
 import "../buttonStyle.css";
+import emailjs from "@emailjs/browser";
 import { styles } from "../style";
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_fcdrz7b",
+        "template_uyx0zmf",
+        form.current,
+        "nQ59IwStphiSqwLAD"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   return (
     <>
       <h1 id="contact" className={`${styles.sectionSubText}`}>
@@ -16,12 +38,13 @@ const Contact = () => {
         <div className="mt-5 lg:mt-0 lg:w-2/3 w-full ">
           <motion.div variants={fadeIn("up", "spring", 0.5, 0.75)}>
             <div className="w-full px-8 py-8 mx-auto overflow-hidden   rounded-lg shadow-2xl     backdrop-blur-sm  ">
-              <form className="mt-6">
+              <form ref={form} onSubmit={sendEmail} className="mt-6">
                 <div className="flex-1">
                   <label className="block mb-2 text-sm      text-gray-200">
                     Full Name
                   </label>
                   <input
+                    name="from_name"
                     type="text"
                     placeholder="name"
                     className="block w-full px-5 py-3 mt-2      border   rounded-md   placeholder-gray-600   bg-gray-900   text-gray-300   border-gray-700    focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
@@ -33,6 +56,7 @@ const Contact = () => {
                   </label>
                   <input
                     type="email"
+                    name="from_email"
                     placeholder="name@example.com"
                     className="block w-full px-5 py-3 mt-2      border   rounded-md   placeholder-gray-600   bg-gray-900   text-gray-300   border-gray-700     focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
                   />
@@ -42,10 +66,11 @@ const Contact = () => {
                     Message
                   </label>
                   <textarea
+                    name="message"
                     className="block w-full h-32 px-5 py-3 mt-2      border   rounded-md md:h-48   placeholder-gray-600   bg-gray-900   text-gray-300   border-gray-700    focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
                     placeholder="Message"></textarea>
                 </div>
-                <button class="button-85" role="button">
+                <button type="submit" class="button-85" role="button">
                   Send Message
                 </button>
               </form>
